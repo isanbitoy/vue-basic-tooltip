1aadf59a6df0fab67b9e77fcc10bdbb5d822ae45
isan <38833845+isanbitoy@users.noreply.github.com>
2020-02-16 22:59:27 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,8 @@
 module.exports = {
-	base: '/',
+	base: '/vue-basic-tooltip/',
 	title: 'Vue Basic Tooltip',
 	description: 'a basic vuejs based tooltip',
 	themeConfig: {
-		repo: '/vue-basic-tooltip',
-		docsDir: '/docs/',
 		search: false
 	}
 }
