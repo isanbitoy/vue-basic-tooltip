bd678a486ce82ee39bb8378ab26de64682aa2bf7
isan <isanbitoy@gmail.com>
2020-02-17 21:53:55 +0800

    add folder

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	base: '/vue-basic-tooltip/',
+	title: 'Vue Basic Tooltip',
+	description: 'a basic vuejs based tooltip',
+	dest: './docs',
+	themeConfig: {
+		search: false
+	}
+}
