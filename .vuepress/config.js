15b70fe33e21039a7286bf56957cc8da6118df9e
isan <isanbitoy@gmail.com>
2020-02-18 21:21:13 +0800

    clean repo

--- .vuepress/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	base: '/vue-basic-tooltip/',
-	title: 'Vue Basic Tooltip',
-	description: 'a basic vuejs based tooltip',
-	dest: './docs',
-	themeConfig: {
-		search: false
-	}
-}
